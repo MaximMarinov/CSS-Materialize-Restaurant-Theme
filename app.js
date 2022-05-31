6508d882ea47ba5e89e4d070b2b3a2d0394cfb29
MaximMarinov <maximmarinovv@gmail.com>
2022-05-31 22:42:37 +0300

    Search Seaction; Autocomplete

--- app.js
+++ app.js
@@ -8,7 +8,25 @@ const slider = document.querySelector('.slider');
 // eslint-disable-next-line no-undef
 M.Slider.init(slider, {
     indicators: false,
-    height: 500,
+    height: 400,
     transition: 500,
     interval: 5000
+});
+
+//AUTOCOMPLETE
+const autocomplete = document.querySelector('.autocomplete');
+// eslint-disable-next-line no-undef
+M.Autocomplete.init(autocomplete, {
+    data: {
+        "Pizza Peperone": null,
+        "Pasta Carbonara": null,
+        "Medium Rare Steak": null,
+        "Sushi Alaska": null,
+        "Pasta Bolognese": null,
+        "Cheeseburger": null,
+        "Ceaser Salad": null,
+        "Cheese Cake": null,
+        "Red Velvet Cake": null,
+        "Seafood Risotto": null,
+    }
 });
